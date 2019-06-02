7ae98fb57a729f2e7bc77a72e167e961394b6831
Hamid Javadi <javadi.freelancer@gmail.com>
2019-06-02 14:56:21 +0430

    Add generate method to class

--- src/guid.php
+++ src/guid.php
@@ -5,8 +5,18 @@ namespace Hamidjavadi;
 
 class guid {
 
-   static function test() {
-			return "Hello World";
-   }
+    static function generate() {
+
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+        $hyphen = chr(45);
+        $uuid = substr($charid, 0, 8).$hyphen
+                .substr($charid, 8, 4).$hyphen
+                .substr($charid,12, 4).$hyphen
+                .substr($charid,16, 4).$hyphen
+                .substr($charid,20,12);
+
+        return $uuid;
+
+    }
 
 }
